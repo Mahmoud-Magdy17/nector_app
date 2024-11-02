56229b19eee5ccb7bcc881d222c4ff4f17011867
Mahmoud-Magdy17 <MahmoudMagdy176203@gmail.com>
2024-11-02 13:22:46 +0200

    create two app bars

--- lib/features/home/presentation/views/widgets/custom_home_view_appbar.dart
+++ lib/features/home/presentation/views/widgets/custom_home_view_appbar.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:nector_app/core/utils/app_assets.dart';
 
 class CustomMainHomeViewAppBar extends StatelessWidget {
   const CustomMainHomeViewAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical:  20.0),
+      child: SizedBox(
+        height: 40,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              AppAssets.carrot,
+              fit: BoxFit.contain,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
